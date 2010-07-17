ee79342a21b2cfff389bb8a01d91144cbfb1a0d6
Anastas <anastas.stoyanovsky@gmail.com>
2010-07-17 19:55:30 +0000

    ode.c and ode.h are now obsolete

--- ode.h
+++
@@ -1,7 +0,0 @@
-#ifndef ODE_H
-#define ODE_H
-
-int hh_ode (double t, const double y[], double f[], void *params);
-int ode_run(struct network * network, double t, double t1, double step_size, double error);
-
-#endif
